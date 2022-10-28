08de833f165dd3ec8c8e0ce3e7b9433f78d77b96
Matija Gašpar <matija.gaspar@gmail.com>
2022-10-28 10:39:25 +0200

    chore: switching from cra and rollup to vite (#22)
    * chore: switching from cra and rollup to vite
    
    * chore: update index.html
    
    * chore: vite build add cjs and es

---
+++ vite.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    svgr()
+  ],
+  esbuild: {
+    // We use function names for generating readable error messages, so we want
+    // them to be preserved when building and minifying.
+    keepNames: true
+  },
+  build: {
+    minify: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: 'src/export.js',
+      fileName: format => `${format}/index.js`,
+      name: 'federationInfo',
+      formats: ['umd', 'cjs', 'es']
+    },
+    rollupOptions: {
+      external: ['react', '@graphiql/react', 'graphql', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          '@graphiql/react': 'GraphiQL.React',
+          graphql: 'GraphiQL.GraphQL'
+        }
+      }
+    }
+  }
+})
