eb92c15728e63e17b003429d1627824116636e66
Andrea Forni <andrea.forni@nearform.com>
2022-12-07 13:00:48 +0100

    chore: remove console.log

--- src/federation-info-plugin/views/ServicesView/ServicesView.jsx
+++ src/federation-info-plugin/views/ServicesView/ServicesView.jsx
@@ -9,22 +9,19 @@ import ServiceInfo from '../../components/ServiceInfo/ServiceInfo'
  * @param {Object} props.federationServices result of prepareServicesViewData
  * @returns  {JSX.Element}
  */
-const ServicesView = ({ federationServices }) => {
-  console.log('federationServices', federationServices)
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minWidth: '200px' }}>
-      <PanelTitle total={federationServices.length}>Services</PanelTitle>
-      <Box sx={{ overflow: 'auto', height: '82vh', paddingRight: 2 }}>
-        {federationServices.map(({ serviceName, itemsMap }) => (
-          <ServiceInfo
-            key={serviceName}
-            serviceName={serviceName}
-            itemsMap={itemsMap}
-          />
-        ))}
-      </Box>
+const ServicesView = ({ federationServices }) => (
+  <Box sx={{ display: 'flex', flexDirection: 'column', minWidth: '200px' }}>
+    <PanelTitle total={federationServices.length}>Services</PanelTitle>
+    <Box sx={{ overflow: 'auto', height: '82vh', paddingRight: 2 }}>
+      {federationServices.map(({ serviceName, itemsMap }) => (
+        <ServiceInfo
+          key={serviceName}
+          serviceName={serviceName}
+          itemsMap={itemsMap}
+        />
+      ))}
     </Box>
-  )
-}
+  </Box>
+)
 
 export default ServicesView
