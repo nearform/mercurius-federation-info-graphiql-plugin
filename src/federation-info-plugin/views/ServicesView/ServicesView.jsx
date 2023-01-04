bb399945f05b848e887316a55cebc8557d6cbf52
Matija Gašpar <matija.gaspar@gmail.com>
2023-01-04 16:47:06 +0100

    feat: automatic expansion and collapse of services tree (#133)
    * feat: automatic expansion and collapse of services tree, based on search results
    
    * feat: save search query into localstorage

--- src/federation-info-plugin/views/ServicesView/ServicesView.jsx
+++ src/federation-info-plugin/views/ServicesView/ServicesView.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import { Box } from '@mui/material'
 
 import PanelTitle from '../../components/PanelTitle/PanelTitle'
@@ -6,19 +6,37 @@ import ServiceInfo from '../../components/ServiceInfo/ServiceInfo'
 import SearchServiceInput from '../../components/SearchServiceInput/SearchServiceInput'
 import filterServicesInfo from '../../utils/filterServicesInfo'
 import { useDebounce } from 'use-debounce'
+import { usePluginState } from '../../context/PluginState'
 /**
  *
  * @param {Object} props.federationServices result of prepareServicesViewData
  * @returns {JSX.Element}
  */
 const ServicesView = ({ federationServices }) => {
-  const [searchText, setSearchText] = useState('')
+  const { setTreeOpenState, setServicesSearchQuery, searchServicesQuery } =
+    usePluginState()
+  const [searchText, setSearchText] = useState(searchServicesQuery)
   const [debouncedSearchText] = useDebounce(searchText, 150)
 
-  const filteredServices = useMemo(
-    () => filterServicesInfo(federationServices, debouncedSearchText),
-    [federationServices, debouncedSearchText]
-  )
+  const { filteredServices, treeOpenState } = useMemo(() => {
+    return filterServicesInfo(federationServices, debouncedSearchText)
+  }, [federationServices, debouncedSearchText])
+
+  useEffect(() => {
+    if (searchServicesQuery !== debouncedSearchText) {
+      setServicesSearchQuery(debouncedSearchText)
+    }
+    //not including the setServicesSearchQuery, should never change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchText])
+
+  useEffect(() => {
+    if (searchText && treeOpenState) {
+      setTreeOpenState(treeOpenState)
+    }
+    // not including the setTreeOpenState, it should not trigger the hook
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [treeOpenState])
 
   const services = debouncedSearchText ? filteredServices : federationServices
 
