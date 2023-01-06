5913ea2a0e637fcc201385da3314c1f8acb53c15
Claudia Donea <claudia.donea@nearform.com>
2023-01-06 14:57:16 +0100

    feat: wait for network test 2

--- cypress/e2e/plugin.cy.js
+++ cypress/e2e/plugin.cy.js
@@ -3,7 +3,7 @@ import 'cypress-network-idle'
 describe('plugin page', () => {
   it('navigates to plugin and has services displayed', () => {
     cy.visit('/graphiql')
-    cy.waitForNetworkIdle('GET', 'https://unpkg.com/*', 1000)
+    cy.waitForNetworkIdle('GET', 'https://unpkg.com/(react|graphiql)*', 1000)
     cy.get('[aria-label="Show Federation info explorer"]').click()
     cy.contains('Federation Info')
     cy.contains('Services')
