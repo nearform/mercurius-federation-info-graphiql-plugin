7fec0525f223db6222b31bf6516e417a89aa4a04
Claudia Donea <claudia.donea@nearform.com>
2023-01-06 17:47:56 +0100

    feat: put back static wait :(

--- cypress/e2e/plugin.cy.js
+++ cypress/e2e/plugin.cy.js
@@ -1,8 +1,7 @@
 describe('plugin page', () => {
   it('navigates to plugin and has services displayed', () => {
     cy.visit('/graphiql')
-    cy.resolve('GraphiQL')
-    cy.resolve('React')
+    cy.wait(2000)
     cy.get('[aria-label="Show Federation info explorer"]').click()
     cy.contains('Federation Info')
     cy.contains('Services')
