b0b60a18533ae099da580fae6e1736b0f66177cb
Claudia Donea <claudia.donea@nearform.com>
2023-01-06 13:10:31 +0100

    feat: increase wait :(

--- cypress/e2e/plugin.cy.js
+++ cypress/e2e/plugin.cy.js
@@ -1,6 +1,6 @@
 describe('plugin page', () => {
   it('navigates to plugin and has services displayed', () => {
-    cy.visit('/graphiql').wait(500) // need to wait for all the unpkg react resorces to load
+    cy.visit('/graphiql').wait(1000) // need to wait for all the unpkg react resorces to load
     cy.get('[aria-label="Show Federation info explorer"]').click()
     cy.contains('Federation Info')
     cy.contains('Services')
