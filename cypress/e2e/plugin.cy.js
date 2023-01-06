52f17db04d0982a0624a0e6af935079a0f0c7c8e
Claudia Donea <claudia.donea@nearform.com>
2023-01-06 18:03:08 +0100

    feat: decrease time

--- cypress/e2e/plugin.cy.js
+++ cypress/e2e/plugin.cy.js
@@ -1,7 +1,7 @@
 describe('plugin page', () => {
   it('navigates to plugin and has services displayed', () => {
     cy.visit('/graphiql')
-    cy.wait(2000)
+    cy.wait(1000)
     cy.get('[aria-label="Show Federation info explorer"]').click()
     cy.contains('Federation Info')
     cy.contains('Services')
