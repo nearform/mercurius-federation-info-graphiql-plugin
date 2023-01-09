9f15a78fc16dcf8577e28b863185c57e4a7d3062
DimplePatel <DimplePatel@users.noreply.github.com>
2023-01-09 15:18:49 +0000

    Using an alias to wait for graphql request
    Removing the cy.wait(3000) and replacing it with a wait for the /graphql request

--- cypress/e2e/plugin.cy.js
+++ cypress/e2e/plugin.cy.js
@@ -1,7 +1,8 @@
 describe('plugin page', () => {
   it('navigates to plugin and has services displayed', () => {
+    cy.intercept('graphql').as('graphql')
     cy.visit('/graphiql')
-    cy.wait(3000)
+    cy.wait('@graphql')
     cy.get('[aria-label="Show Federation info explorer"]').click()
     cy.contains('Federation Info')
     cy.contains('Services')
