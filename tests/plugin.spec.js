d0281ec8c283968577dbdc680805604187e48b82
Dimple Patel <dimple.patel@nearform.com>
2023-01-11 16:30:02 +0000

    feat: more tests

--- tests/plugin.spec.js
+++ tests/plugin.spec.js
@@ -1,11 +1,22 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 const { test, expect } = require('@playwright/test')
 
-test('visits URL', async ({ page }) => {
+test('checks Federation Info headings and buttons', async ({ page }) => {
   await page.goto('http://localhost:3001/graphiql')
-  //click Federation Info explorer button
-  page.getByTestId('plugin-icon').click()
+  //clicks Federation Info explorer button
+  await page.getByTestId('plugin-icon').click()
   await expect(page.getByText('Federation Info')).toBeVisible()
   await expect(page.getByRole('heading', { name: 'Services' })).toBeVisible()
-  await expect(page.getByText('Welcome to GraphiQL')).toBeVisible()
+  await expect(
+    page.getByRole('heading', { name: 'Unified schema' })
+  ).toBeVisible()
+  await expect(page.getByRole('button', { name: 'user service' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'post service' })).toBeVisible()
+  await expect(
+    page.getByRole('heading', { name: 'Unified schema' })
+  ).toBeVisible()
+  await page.getByText('heading', { name: 'Queries' }).isVisible()
+  await page.getByText('heading', { name: 'Mutations' }).isVisible()
+  await page.getByText('heading', { name: 'Subscriptions' }).isVisible()
+  await page.getByText('heading', { name: 'Types' }).isVisible()
 })
