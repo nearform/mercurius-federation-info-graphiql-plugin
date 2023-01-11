aa6da58b7b150ee497598178790812aeb0f35908
Dimple Patel <dimple.patel@nearform.com>
2023-01-11 13:39:01 +0000

    feat: first test again

--- tests/plugin.spec.js
+++ tests/plugin.spec.js
@@ -1,11 +1,11 @@
-// @ts-check
+/* eslint-disable testing-library/prefer-screen-queries */
 const { test, expect } = require('@playwright/test')
 
-test('visits URL', async render => {
-  await render.page.goto('http://localhost:3001/graphiql')
+test('visits URL', async ({ page }) => {
+  await page.goto('http://localhost:3001/graphiql')
   //click Federation Info explorer button
-  render.page.getByTestId('plugin-icon').click()
-  await expect(render.page.getByText('Federation Info')).toBeVisible()
-  //await expect(render.page.getByRole('heading', { name: 'Services' })).toBeVisible();
-  await expect(render.page.getByText('Welcome to GraphiQL')).toBeVisible()
+  page.getByTestId('plugin-icon').click()
+  await expect(page.getByText('Federation Info')).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Services' })).toBeVisible()
+  await expect(page.getByText('Welcome to GraphiQL')).toBeVisible()
 })
